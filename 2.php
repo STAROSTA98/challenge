9851bc07064a94f823747602016415304b97c24c
shckonda@inbox.ru <RjNvJM#6E3Q47d5X>
2021-08-11 14:05:15 +0300

    2 задание mysortkey

--- 2.php
+++ 2.php
@@ -44,5 +44,117 @@ echo "<pre> отсортированный массив \n";
 print_r(mySortForKey($arr, 'b'));
 echo "</pre>";
 
+function conMySQL()
+{
+    $host = 'localhost';
+    $db = 'test_samson';
+    $user = 'root';
+    $pass = '';
+    $charset = 'utf8';
+
+    $dsn = "mysql:host=$host;dbname=$db;charset=$charset";
+    $opt = [
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+        PDO::ATTR_EMULATE_PREPARES => false
+    ];
+
+    return new PDO($dsn, $user, $pass, $opt);
+}
+
+function importXml($a)
+{
+    $pdo = conMySQL();
+
+
+    function setIns($val, $fields = [])
+    {
+
+        if (!empty($fields)) {
+            $extSql = '(';
+            foreach ($fields as $field) {
+                $extSql .= $field . ',';
+            }
+            $extSql = substr_replace($extSql, ') VALUES (', -1);
+            foreach ($val as $item) {
+                is_string($item) ? $extSql .= "'" . $item . "'," : $extSql .= $item . ",";
+            }
+            $extSql = substr_replace($extSql, ');', -1);
+        } else {
+            $extSql = 'VALUES (';
+            foreach ($val as $item) {
+                is_string($item) ? $extSql .= "'" . $item . "'," : $extSql .= $item . ",";
+            }
+            $extSql = substr_replace($extSql, ');', -1);
+        }
+
+        return $extSql;
+    }
+
+    if (file_exists($a)) {
+        $xml = simplexml_load_file($a);
+    }
+
+    foreach ($xml as $tovar) {
+
+        $arrVal = [$tovar['Код'], (string)$tovar['Название']];
+        $arrFields = ['kod', 'name'];
+        $sql = 'INSERT INTO a_product ' . setIns($arrVal, $arrFields);
+        $stm = $pdo->prepare($sql);
+        $stm->execute();
+
+        $idTovar = $pdo->lastInsertId();
+
+        $arrVal = [];
+        $arrFields = ['tovar', 'propety'];
+
+        foreach ($tovar->Свойства as $property) {
+
+            $arrVal = [$idTovar, json_encode($property, JSON_UNESCAPED_UNICODE)];
+
+        }
+
+        $sql = 'INSERT INTO a_property ' . setIns($arrVal, $arrFields);
+        $stm = $pdo->prepare($sql);
+        $stm->execute();
+
+        $arrFields = ['tovar', 'type_price', 'price'];
+
+        foreach ($tovar->Цена as $key => $item) {
+            $arrVal = [$idTovar, (string)$item['Тип'], (float)$item];
+            $sql = 'INSERT INTO a_price ' . setIns($arrVal, $arrFields);
+            $stm = $pdo->prepare($sql);
+            $stm->execute();
+        }
+
+        foreach ($tovar->Разделы as $category) {
+            foreach ($category as $item) {
+
+                $arrFields = ['kod', 'name', 'parent_kod'];
+
+                if ($item['Родитель']) {
+                    $arrVal = [$item['Код'], (string)$item, $item['Родитель']];
+                } else {
+                    $arrVal = [$item['Код'], (string)$item, 'NULL'];
+                }
+                $sql = 'INSERT IGNORE INTO a_category ' . setIns($arrVal, $arrFields);
+                $stm = $pdo->prepare($sql);
+                $stm->execute();
+
+                $arrVal = [$tovar['Код'], $item['Код']];
+                $arrFields = ['tovar', 'category'];
+                $sql = 'INSERT INTO product_category ' . setIns($arrVal, $arrFields);
+                $stm = $pdo->prepare($sql);
+                $stm->execute();
+
+            }
+
+        }
+
+    }
+
+}
+
+importXml('tovar.xml');
 
 ?>
