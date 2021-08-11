5318e0fa730e88503de65ea6fcc13ccc6ac1cd82
shckonda@inbox.ru <RjNvJM#6E3Q47d5X>
2021-08-11 14:01:27 +0300

    2 задание mysortkey

--- 2.php
+++ 2.php
@@ -14,6 +14,35 @@ function convertString($a, $b)
 echo "Мама мыла раму мама мама <br>";
 echo convertString("Мама мыла раму мама она мама", "мама");
 
+function mySortForKey($a, $b)
+{
+    $idErr = -1;
+
+    foreach ($a as $key => $item) {
+        !array_key_exists($b, $item) ? $idErr = $key : false;
+    }
+
+    if ($idErr >= 0) {
+        throw new Exception('В подмассиве ' . $idErr . ' отсутствует ключ "' . $b . '"');
+    } else {
+        uasort($a, function ($i, $j) use ($b, $a) {
+            return $i[$b] > $j[$b] ? 1 : 0;
+        });
+    }
+
+    return $a;
+}
+
+
+$arr =  [['a' => 2, 'b' => 33], ['b' => 34], ['a' => 77, 'b' => 55], ['a' => 54, 'b' => 65], ['a' => 4455, 'b' => 563], ['a' => 57, 'b' => 0], ['a' => 9, 'b' => 1055]];
+
+echo "<pre> исходный массив \n";
+print_r(mySortForKey($arr, 'b'));
+echo "</pre>";
+
+echo "<pre> отсортированный массив \n";
+print_r(mySortForKey($arr, 'b'));
+echo "</pre>";
 
 
 ?>
